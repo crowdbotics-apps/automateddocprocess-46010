a665005d733b9aeb3c9288ae2c743e93fce144c2
Crowdbotics <team@crowdbotics.com>
2024-02-05 22:14:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "automateddocprocess-46010",
+  url: "https://automateddocprocess-46010.botics.co",
+  api: "https://automateddocprocess-46010.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
